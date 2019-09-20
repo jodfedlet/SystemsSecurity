a41035bb4d49ac649cf7a14f8401ce720c3045c6
Your Name <you@example.com>
2019-09-19 23:19:52 -0300

    Cifrar mensagens

--- RSA.py
+++ RSA.py
@@ -26,16 +26,31 @@ def calcularE(fiN,e):
     else:
         e += 1
         return calcularE(fiN,e)
-            
+
+def cifrarMes(n, e):
+    message = input('Enter a message: ')
+    messLower = message.lower()
+    cifrar = []
+    for let in messLower:
+        asc = ord(let)
+        cryp = asc**e % n
+        cifrar.append(cryp)
+    print('Mensagem Cifrada: ', end="") 
+    for cif in cifrar:
+        print('{} '.format(cif), end="")   
+    print()  
+
+import string as str          
 p = int(input('Enter a number: '))
 q = int(input('Enter a secund number: '))
 n = p * q
 fiN = (p - 1) * (q - 1)
-
-print('N: ',n)
-print('Fi de n: ',fiN)
 e = calcularE(fiN, 2)
-print('e: ',e)
+print('Chave publica: ({} , {})'.format(n,e))
+
+cifrarMes(n,e)
+
+    
 
 
 
