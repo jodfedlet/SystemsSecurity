89447feb4f91549522a4f35a658b2efc41fc9cb7
Your Name <you@example.com>
2019-11-16 18:51:30 -0300

    Finalizando a cifragem do César

--- cifra_de_cesar.py
+++ cifra_de_cesar.py
@@ -1,5 +1,6 @@
 def cifrar(msg, cv):
-
+	cesar = lambda char, cv, alf=26: chr((ord(char) - ord('a') + cv)%alf + ord('a'))
+	return ''.join([cesar(char, cv) for char in msg.lower()])
 
 mensagem = input('Digite a mensagem: ')
 chave 	 = int(input('Digite a chave da codificacao: '))
