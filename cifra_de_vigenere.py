70e983b795cc0118ee29d1b5528d3e3cecd67ce2
Your Name <you@example.com>
2019-11-17 20:23:29 -0300

    Atualizando a cifra de Vigenere

--- cifra_de_vigenere.py
+++ cifra_de_vigenere.py
@@ -1,6 +1,6 @@
 
-def cifrar(msg, key, alfabeto, opcao):
-	key_len, key_aux, msg_aux = len(key), [ord(i) for i in key], [ord(j) for j in msg]
+def vigenereCypher(msg, key, alfabeto, opcao):
+	key_len, key_aux, msg_aux = len(key), [ord(i) for i in key], [ord(i) for i in msg]
 	msg_len, alf_len = len(msg_aux), len(alfabeto)
 
 	res = ''
@@ -8,18 +8,31 @@ def cifrar(msg, key, alfabeto, opcao):
 		value = 0
 		if(opcao == 1):
 			value = (msg_aux[i] + key_aux[i%key_len]) % alf_len
-		if(opcao == 2):
-			value = (msg_aux[i] - key_aux[i%key_len]) % alf_len
+		# if(opcao == 2):
+		# 	value = (msg_aux[i] - key_aux[i%key_len]) % alf_len
 		res += chr(value + ord(alfabeto[0])) 
-	return res			
+	return res	
+
+def findKey(msg, key):	
+	key_len = len(key)
+	new_key = []
+	for i in range(len(msg)):
+		new_key.append(key[i%key_len])
+	return new_key
 
 import string as stg
 key = 'LIMAO'
 msg = 'ATACARBASESUL' 
 alf = list(stg.ascii_lowercase)
-cifra_de_vigenere = cifrar(msg, key, alf,1)
+
+KEY = ''.join(findKey(msg, key))
+
+print('A chave: ',KEY)
+
+cifra_de_vigenere = vigenereCypher(msg, KEY, alf,1)
 print('A cifragem de Vigenere: ',cifra_de_vigenere)
-decifra_de_vigenere = cifrar(msg, key, alf,2)
-print('A decifragem de Vigenere: ',decifra_de_vigenere)
+
+# decifra_de_vigenere = vigenereCypher(cifra_de_vigenere, KEY, alf,2)
+# print('A decifragem de Vigenere: ',decifra_de_vigenere)
 
 
